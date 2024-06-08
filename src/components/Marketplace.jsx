51611661337fa837968b1753f6799743b2dd9536
Jaachi Okafor <okaforjaachi@gmail.com>
2024-06-08 03:18:34 +0100

    added Marketplace section

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -3,6 +3,47 @@ import skillsIcon from "../assets/svg/skills.svg";
 import profilesIcon from "../assets/svg/profiles.svg";
 import subCategoriesIcon from "../assets/svg/subCategories.svg";
 
+import shopify from "../assets/svg/marketplace/shopify.svg";
+import magento from "../assets/svg/marketplace/magento.svg";
+import data from "../assets/svg/marketplace/data.svg";
+import webflow from "../assets/svg/marketplace/webflow.svg";
+import dotnet from "../assets/svg/marketplace/dotnet.svg";
+import zwilt from "../assets/svg/marketplace/zwilt.svg";
+
+const itAndDevelopment = [
+  {
+    desc1: "Shopify",
+    desc2: "Developer",
+    logo: shopify,
+  },
+  {
+    desc1: "Magento",
+    desc2: "Developer",
+    logo: magento,
+  },
+  {
+    desc1: "Data",
+    desc2: "Scientist",
+    logo: data,
+  },
+  {
+    name: "Webflow Developer",
+    desc1: "Webflow",
+    desc2: "Developer",
+    logo: webflow,
+  },
+  {
+    name: "Dotnet Developer",
+    logo: dotnet,
+    desc1: "Dotnet",
+    desc2: "Developer",
+  },
+  {
+    name: "",
+    logo: zwilt,
+  },
+];
+
 export const Marketplace = () => {
   return (
     <div className="bg-[#EDEFFF] -skew-y-2">
@@ -15,11 +56,11 @@ export const Marketplace = () => {
           </p>
         </div>
         {/* writeup */}
-        <div>
+        <div className="flex flex-col justify-center">
           {/* IT and Development */}
-          <div>
-            <div>
-              <p className="text-xl">
+          <div className="flex justify-around space-x-32 mb-10 ">
+            <div className="">
+              <p className="text-xl mt-4 mb-6">
                 Find Dev and IT professionals to <br /> scale your business.
               </p>
               <div className="grid grid-cols-2">
@@ -40,10 +81,60 @@ export const Marketplace = () => {
                 </div>
               </div>
             </div>
-            <div></div>
+            <div className="bg-white p-5 rounded-lg">
+              <p className="mb-6">IT & Development</p>
+              <div className=" flex space-x-14">
+                {itAndDevelopment.map((item, index) => (
+                  <div className=" text-center">
+                    <div className=" bg-[#F6F6F6] rounded-full p-8">
+                      <img src={item.logo} alt="" className="h-8" />
+                    </div>
+                    <p>{item.desc1}</p>
+                    <p>{item.desc2}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
           {/* Design and Creative */}
-          <div></div>
+          <div className="flex justify-around space-x-32 ">
+            <div className="">
+              <p className="text-xl mt-4 mb-6">
+                Explore Creative individuals with a <br /> keen eye for detail.
+              </p>
+              <div className="grid grid-cols-2">
+                {/* skills */}
+                <div className="flex space-x-3 items-center">
+                  <img src={skillsIcon} alt="" />
+                  <p>989 Skills</p>
+                </div>
+                {/* sub categories */}
+                <div className="flex space-x-3 items-center">
+                  <img src={subCategoriesIcon} alt="" />
+                  <p>45 Sub-Categories</p>
+                </div>
+                {/* profiles */}
+                <div className="flex space-x-3 items-center">
+                  <img src={profilesIcon} alt="" />
+                  <p>1011 Profiles</p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white p-5 rounded-lg">
+              <p className="mb-6">Design & Creative</p>
+              <div className=" flex space-x-14">
+                {itAndDevelopment.map((item, index) => (
+                  <div className=" text-center">
+                    <div className=" bg-[#F6F6F6] rounded-full p-8">
+                      <img src={item.logo} alt="" className="h-8" />
+                    </div>
+                    <p>{item.desc1}</p>
+                    <p>{item.desc2}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
